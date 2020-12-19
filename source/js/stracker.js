dec4e21d32c73efa17c549c4b453c19ed5d0c68f
fedeghe <fedeghe@gmail.com>
2020-12-20 00:25:54 +0100

    scroll.. also needs debounce sure

--- source/js/stracker.js
+++ source/js/stracker.js
@@ -4,6 +4,11 @@ document.body.addEventListener('click', function (e) {
     console.log(e.target)
     console.log('-----------\n\n')
 }, {capture: true})
+window.addEventListener('scroll', function (e) {
+    console.log('+++++++++++')
+    console.log(e)
+    console.log('-----------\n\n')
+}, {capture: true})
 
 
 document.getElementById('xxx').addEventListener('click', function (e) {
